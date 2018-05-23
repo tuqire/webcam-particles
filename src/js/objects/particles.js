8fa9df5dfc27287e8bf4a0afdc7ea6ebab009c8c
Tuqire Hussain <me@tuqire.com>
2018-05-23 04:10:58 +0100

    got image difference working

--- src/js/objects/particles.js
+++ src/js/objects/particles.js
@@ -116,8 +116,6 @@ export default class Particles {
     videoDiffTexture.minFilter = videoDiffTexture.magFilter = THREE.NearestFilter
     videoDiffTexture.needsUpdate = true
 
-    document.getElementsByTagName('body')[0].prepend(videoDiffImage)
-
     const material = this.material = new THREE.ShaderMaterial({
       blending: THREE.NormalBlending,
       uniforms: Object.assign({}, this.uniforms, {
@@ -183,13 +181,33 @@ export default class Particles {
 
         const imgPixels = videoImageContext.getImageData(0, 0, videoWidth, videoHeight)
 
+        for (let y = 0; y < imgPixels.height; y++) {
+          for (let x = 0; x < imgPixels.width; x++) {
+            const i = (y * 4) * imgPixels.width + x * 4
+            const avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3
+
+            imgPixels.data[i] = avg
+            imgPixels.data[i + 1] = avg
+            imgPixels.data[i + 2] = avg
+          }
+        }
+
         for (let y = 0; y < imgPixels.height; y += 1) {
           for (let x = 0; x < imgPixels.width; x += 1) {
             const i = (y * 4) * imgPixels.width + x * 4
 
-            imgPixels.data[i] -= imgPixels.data[i - 3]
-            imgPixels.data[i + 1] -= imgPixels.data[i - 2]
-            imgPixels.data[i + 2] -= imgPixels.data[i - 1]
+            const average = (imgPixels.data[i - 3] + imgPixels.data[i + 5] +
+              imgPixels.data[i - (imgPixels.width * 4) + 1] + imgPixels.data[i + (imgPixels.width * 4) + 1] +
+              imgPixels.data[i - (imgPixels.width * 4) - 3] + imgPixels.data[i + (imgPixels.width * 4) - 3] +
+              imgPixels.data[i - (imgPixels.width * 4) + 5] + imgPixels.data[i + (imgPixels.width * 4) + 5]) / 4
+
+            imgPixels.data[i] -= average
+            imgPixels.data[i + 1] -= average
+            imgPixels.data[i + 2] -= average
+
+            imgPixels.data[i] *= 20
+            imgPixels.data[i + 1] *= 20
+            imgPixels.data[i + 2] *= 20
           }
         }
 
